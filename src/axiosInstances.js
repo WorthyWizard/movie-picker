67e6f0b4d2f6aedb78f760b90ad3825bea98c0b9
WorthyWizard <amilogames@gmail.com>
2021-03-31 08:32:02 +0300

    improving security

--- src/axiosInstances.js
+++ src/axiosInstances.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = 'ffd060a06690d2a4bc351e7e1e820854';
+const apiKey = process.env.REACT_APP_API_KEY;
 
 export const axiosMovies = axios.create({
   baseURL: `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}`
